6515540234e0ae973f6c858c91e83c070567f6c4
AO Industries, Inc <support@aoindustries.com>
2021-10-07 07:25:44 -0500

    Using method reference instead of lambda

--- src/main/java/com/pragmatickm/task/renderer/html/TaskUtil.java
+++ src/main/java/com/pragmatickm/task/renderer/html/TaskUtil.java
@@ -170,7 +170,7 @@ public final class TaskUtil {
 		return cache.getAttribute(
 			GET_STATUS_CACHE_KEY,
 			Map.class,
-			() -> cache.newMap()
+			cache::newMap
 		);
 	}
 
@@ -1068,7 +1068,7 @@ public final class TaskUtil {
 		Map<PageUserKey, V> pageUserCache = cache.getAttribute(
 			key,
 			Map.class,
-			() -> cache.newMap()
+			cache::newMap
 		);
 		return pageUserCache;
 	}
