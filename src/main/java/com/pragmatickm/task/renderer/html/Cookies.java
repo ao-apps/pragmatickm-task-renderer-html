d64d99be84b016e28c0e6fd2925e0fa32f3d3de3
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:03:11 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/pragmatickm/task/renderer/html/Cookies.java
+++ src/main/java/com/pragmatickm/task/renderer/html/Cookies.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with pragmatickm-task-renderer-html.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.pragmatickm.task.renderer.html;
 
 import static com.aoapps.servlet.http.Cookies.addCookie;
--- src/main/java/com/pragmatickm/task/renderer/html/StatusResult.java
+++ src/main/java/com/pragmatickm/task/renderer/html/StatusResult.java
@@ -1,6 +1,6 @@
 /*
  * pragmatickm-task-renderer-html - Tasks rendered as HTML in a Servlet environment.
- * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with pragmatickm-task-renderer-html.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.pragmatickm.task.renderer.html;
 
 import com.aoapps.lang.util.UnmodifiableCalendar;
